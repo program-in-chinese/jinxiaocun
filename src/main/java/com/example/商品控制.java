b8ff87c21a2d6e1eb3b4a3b54a88a9bc34a91ae0
fromwheretowhere <fromwheretowhere.service@gmail.com>
2016-03-31 23:17:10 -0700

    autowired可以用于私有变量,省去构造方法传入

--- src/main/java/com/example/商品控制.java
+++ src/main/java/com/example/商品控制.java
@@ -15,12 +15,8 @@ public class 商品控制 {
 
 	private static final String 商品表URL = "商品表";
 
-	private 商品库 商品库;
-	
 	@Autowired
-	public 商品控制(商品库 商品库) {
-		this.商品库 = 商品库;
-	}
+	private 商品库 商品库;
 	
 	@RequestMapping(value="/商品表", method=RequestMethod.GET)
 	public String 列表(Model 模型) {
