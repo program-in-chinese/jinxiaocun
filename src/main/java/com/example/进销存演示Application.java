a0d568b3dbf539b945df36512fcfddcd80c46c38
fromwheretowhere <fromwheretowhere.service@gmail.com>
2016-04-03 11:54:36 -0700

    为啥能省去redirect的url encode? 添加test,uri必须用URI对象,否则字符串会被自动encode,导致路径匹配失败; 添加根路径redirect

--- src/main/java/com/example/进销存演示Application.java
+++ src/main/java/com/example/进销存演示Application.java
@@ -1,12 +1,28 @@
 package com.example;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class 进销存演示Application {
+public class 进销存演示Application extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(进销存演示Application.class, args);
 	}
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+		try {
+			registry.addRedirectViewController("/", "/" + URLEncoder.encode(商品控制.URL, "UTF-8"));
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//
+    }
 }
