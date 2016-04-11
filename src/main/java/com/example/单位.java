0e2d9b1219c72346300bc91a72ddf61e5490c9ac
fromwheretowhere <fromwheretowhere.service@gmail.com>
2016-04-10 23:26:13 -0700

    #6 单位:不为空,长度限制

--- src/main/java/com/example/单位.java
+++ src/main/java/com/example/单位.java
@@ -4,11 +4,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 public class 单位 {
 
   private long id;
+
+  @NotEmpty(message = "单位名称不可为空")
+  @Size(max = 20, message = "单位长度不可超过20")
   private String 名称;
 
   @Id
